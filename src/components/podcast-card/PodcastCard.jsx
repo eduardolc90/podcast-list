8724e2eb95dbf38398f53cc324ecb4a2118ad602
eduardolc90 <eduardolc@ext.inditex.com>
2023-03-09 11:24:57 +0100

    PodcastCard test

--- src/components/podcast-card/PodcastCard.jsx
+++ src/components/podcast-card/PodcastCard.jsx
@@ -4,7 +4,7 @@ import { useHistory } from "react-router-dom";
 const PodcastCard = ({ podcasts }) => {
   const history = useHistory();
   return (
-    <div className=" col-md-3 col-12 podcasts-card-container" onClick={() => history.push(`/podcast/${podcasts.id.attributes["im:id"]}`)}>
+    <div className="col-md-3 col-12 podcasts-card-container" onClick={() => history.push(`/podcast/${podcasts.id.attributes["im:id"]}`)}>
       <div className="podcasts-card-container-card">
         <img className="card-img-tops podcasts-card-container-card__img" alt="img" src={podcasts["im:image"][0].label} />
         <div className="card-body">
